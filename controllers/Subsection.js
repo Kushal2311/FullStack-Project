d9d0924befedc4e1771f7f668ec38b2f1fe5f674
Kushal2311 <kushalmalu2311@gmail.com>
2024-03-23 11:43:35 +0530

    done with the changes in subsection controller

--- controllers/Subsection.js
+++ controllers/Subsection.js
@@ -1,4 +1,4 @@
-const subSection = require("../models/SubSection.js");
+const SubSection = require("../models/SubSection.js");
 const Section = require("../models/Section.js");
 const { ApiError } = require("../utils/ApiError.js");
 const { ApiResponse } = require("../utils/ApiResponse");
@@ -41,3 +41,73 @@ exports.createSubSection = async(req , res) => {
         throw new ApiError(404 , "Error while Creating sub-section")
     }
 };
+
+exports.updateSubSection = async(req , res) => {
+    try {
+        const {sectionId , title , description} = req.body ;
+
+        const SubSection = await SubSection.findById(sectionId);
+
+        if(!SubSection){
+            throw new ApiError(404 , "All Fields are required")
+        }
+
+        if(title !== undefined){
+            SubSection.title = title 
+        }
+
+        if(description !== undefined){
+            SubSection.description = description
+        }
+
+        if(req.files && req.files.video !== undefined){
+            const video = req.files.video 
+            const uploadDetails = await uploadImageToCloudinary(
+                video , 
+                process.env.FOLDER_NAME 
+            )
+            SubSection.videoURL = uploadDetails.secure_url 
+            SubSection.timeduration = `${uploadDetails.duration}`
+        }
+
+        await SubSection.save()
+        
+        return res.status(200).json(
+            new ApiResponse(200 , SubSection ,"Sub-Section updated successfully")
+        )
+        
+    } catch (error) {
+        console.log(error);
+        throw new ApiError(404 , "Error while Updating sub-section")
+    }
+};
+
+
+exports.deleteSubSection = async(req , res) => {
+    try {
+        const {subSectionID , sectionId } = req.body ;
+        await Section.findByIdAndUpdate(
+            {_id : sectionId} ,
+            {
+                $pull : {
+                    subSection : subSectionID 
+                },
+            }
+        )
+
+        const subSection = await SubSection.findByIdAndDelete({_id : subSectionID})
+
+        if(!subSection){
+            throw new ApiError(404 , "Sub-Section Not Found")
+        }
+
+        return res.status(200).json(
+            new ApiResponse(200 , SubSection ,"Sub-Section deleted successfully")
+        )
+        
+    } catch (error) {
+        console.log(error);
+        throw new ApiError(404 , "Error while deleting sub-section")
+    }
+}
+
